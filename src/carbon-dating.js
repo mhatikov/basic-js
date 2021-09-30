afe598e7ddc9f7e6c0fd4c502c2f6a8f52545458
Mihail Kramskov <kramskov.m@yandex.ru>
2021-09-30 17:28:12 +0400

    feat: carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,13 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+ if(typeof sampleActivity === 'string' && Number(sampleActivity) !== NaN && Number(sampleActivity) > 0 && Number(sampleActivity) < MODERN_ACTIVITY){
+  const rate = 0.693 / HALF_LIFE_PERIOD;
+  const decay = Math.log(MODERN_ACTIVITY/ Number(sampleActivity));
+
+  return Math.ceil(decay/rate)
+ }else{
+   return false;
+ }
 }
