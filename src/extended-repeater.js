a9242d1851ce57e5fcb058efcbd64a4e2a351ebc
Mihail Kramskov <kramskov.m@yandex.ru>
2021-10-03 15:35:08 +0400

    feat: extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,7 +15,32 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function repeater(str, options) {
+
+  let rep = options.repeatTimes;
+  let result = Array(rep).fill(`${str}`);
+  let sep = options.separator ? options.separator : '+';
+  let addSep = options.additionSeparator ? options.additionSeparator : '|';
+  let addition = options.addition;
+  let additionTimes = options.additionRepeatTimes ? options.additionRepeatTimes : 1;
+
+
+  let newresult = [];
+  
+  if (addition !== undefined) {
+    addition = `${options.addition}`;
+    result.forEach((item) =>{
+      if (additionTimes > 1) {
+        let additionWithSep = Array(additionTimes).fill(addition);
+        additionWithSep = additionWithSep.join(addSep);
+        newresult.push(item += additionWithSep);
+      } else {
+        newresult.push(item += addition.repeat(additionTimes));
+      }
+    })
+    result = newresult;
+  }
+
+  result = sep && rep > 1 ? result.join(sep) : result.join('');
+  return result
 }
